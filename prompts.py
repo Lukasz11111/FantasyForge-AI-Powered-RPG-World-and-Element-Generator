d579514bfb44237ebd8761bdaff04dfc3c4cc7b8
Łukasz Miklas <lukasz@revdebug.com>
2023-08-05 16:46:09 +0200

    add beast prompt && gen beast

--- prompts.py
+++ prompts.py
@@ -6,10 +6,40 @@ import json
 
 placesPath="./prompt/places.json"
 levelPath="./prompt/level.json"
+beastPath="./prompt/beast.json"
+beastAndPlaces='./prompt/beastAndPlaces.json'
+def update_place_with_beasts(place_data, beasts_list):
+    place_name = place_data['name']
+
+    with open(beastAndPlaces, 'r+') as json_file:
+        data = json.load(json_file)
+        places = data.get('places', {})
+
+        if place_name not in places:
+            places[place_name] = []
+
+        places[place_name].extend(beast['name'] for beast in beasts_list)
+
+        json_file.seek(0)
+        json.dump(data, json_file, indent=4)
+        json_file.truncate()
+    
+
+def remove_duplicate_by_name(data_list):
+    name_count = {}
+    result = []
+
+    for item in data_list:
+        name = item.get('name')
+        if name:
+            if name not in name_count:
+                result.append(item)
+                name_count[name] = 1
+
+    return result
 
 def extract_valid_jsons(text):
     valid_jsons = []  # List to store valid JSON objects
-
     start = 0  # Starting index for searching
     while True:
         try:
@@ -19,7 +49,6 @@ def extract_valid_jsons(text):
 
             json_obj = json.loads(json_str)  # Try to parse the JSON string
             valid_jsons.append(json_obj)  # If successful, add to the list
-
             start = end_index  # Move the starting index to the end of the parsed JSON
         except ValueError:
             break  # Stop when no more opening curly braces are found
@@ -30,13 +59,11 @@ def extract_valid_jsons(text):
 
 def filter_json_list(jsonList_, pattern):
     result = []
-
     for item in jsonList_:
         # Check if the current item is a dictionary and all specified keys in the pattern
         # are present in the item and have matching non-empty values.
         if isinstance(item, dict) and all(key in item and item[key] for key, value in pattern.items()):
             result.append(item)
-
     return result
 
 def save_json_to_file(data, filename):
@@ -60,16 +87,41 @@ def places_(count_):
     level=open_json_file(levelPath)
     prompt=f"""using the available levels: 
     {level} 
-    generate {count_} examples. 
+    Generate {count_} examples. 
     Taking as an example: {select_values(places["places"], 10)}
     Remember to include limits for beast_amount {places['range_beast_amount']} and riches taking {places['range_riches']}.
     It's important that you return me only json"""
     completion=PM.chatCompletionCreate(prompt)
+    savePlaces_(completion,places["places"],placesPath)
+    getMonsterToPlace( random.choice(places["places"]))
+    
+def savePlaces_(completion,data,pathFile):
     jsonList_=extract_valid_jsons(str(completion.choices[0].message["content"]))
-    jsonList_=filter_json_list(jsonList_, places["places"][0])
-    places["places"].extend(jsonList_)
-    save_json_to_file(places, placesPath)
-
+    jsonList_=filter_json_list(jsonList_, data[0])
+    data.extend(jsonList_)
+    save_json_to_file(data, pathFile)
+    remove_duplicate_by_name(data)
+    return jsonList_
+    
+def getMonsterToPlace(place): 
+    beast=open_json_file(beastPath)
+    level=open_json_file(levelPath)
+    prompt=f"""
+    Generate or match {place["beast_amount"]} examples
+    Creatures that will match to place '{place["name"]}'
+    Creatures are to be generated according to the given json from which you can match the creatures:
+    {select_values(beast["beast"], 50)}
+    when creating a description, I take care not to attribute the creature to a given region (in this case, {place["beast_amount"]})
+    Generate a json list according to the example ( {place["beast_amount"]} example)
+    (You can also choose a matching spawn from the given list)
+    One last creature must be the boss
+    """
+    completion=PM.chatCompletionCreate(prompt)
+    beasts_list=savePlaces_(completion,beast["beast"],beastPath)
+    update_place_with_beasts(place, beasts_list)
+    
+    
+places_(12)   
 # todo
 
 # monster=''
@@ -80,4 +132,8 @@ def places_(count_):
 # card=''
 # stats=''
 
-places_(20)
+# places=open_json_file(placesPath)
+# getMonsterToPlace( random.choice(places["places"]))
+
+
+# places_(20)
