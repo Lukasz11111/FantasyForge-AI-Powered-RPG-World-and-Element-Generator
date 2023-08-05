699a6d21a63622b8a004b71ae6196c33d814cca8
Łukasz Miklas <lukasz@revdebug.com>
2023-08-05 19:00:55 +0200

    adding the function of combining beasts and places

--- prompts.py
+++ prompts.py
@@ -1,6 +1,7 @@
 import json
 import random
 import promptMaster as PM
+import re
 
 import json
 
@@ -35,12 +36,19 @@ def remove_duplicate_by_name(data_list):
             if name not in name_count:
                 result.append(item)
                 name_count[name] = 1
-
     return result
+
+def fix_apostrophes(text):
+    pattern = r'(?<=[^,:{}[\]\s])"(?=[^,:{}[\]\s])'
+    fixed_text = re.sub(pattern, "'", text)
+    return fixed_text
+
 
 def extract_valid_jsons(text):
     valid_jsons = []  # List to store valid JSON objects
     start = 0  # Starting index for searching
+    text = text.replace("\\'", "'").replace("\\n", "").replace("'", '"').replace("\n", '').replace("False","false").replace("True","true")
+    text=fix_apostrophes(text)
     while True:
         try:
             start_index = text.index('{', start)  # Find the opening curly brace
@@ -62,7 +70,7 @@ def filter_json_list(jsonList_, pattern):
     for item in jsonList_:
         # Check if the current item is a dictionary and all specified keys in the pattern
         # are present in the item and have matching non-empty values.
-        if isinstance(item, dict) and all(key in item and item[key] for key, value in pattern.items()):
+        if isinstance(item, dict) and all(key in item for key, value in pattern.items()):
             result.append(item)
     return result
 
@@ -88,40 +96,41 @@ def places_(count_):
     prompt=f"""using the available levels: 
     {level} 
     Generate {count_} examples. 
-    Taking as an example: {select_values(places["places"], 10)}
+    Taking as an example: {select_values(places['places'], 30)}
     Remember to include limits for beast_amount {places['range_beast_amount']} and riches taking {places['range_riches']}.
     It's important that you return me only json"""
     completion=PM.chatCompletionCreate(prompt)
-    savePlaces_(completion,places["places"],placesPath)
+    savePlaces_(completion,places,placesPath,"places")
     getMonsterToPlace( random.choice(places["places"]))
     
-def savePlaces_(completion,data,pathFile):
+def savePlaces_(completion,data,pathFile,key):
     jsonList_=extract_valid_jsons(str(completion.choices[0].message["content"]))
-    jsonList_=filter_json_list(jsonList_, data[0])
-    data.extend(jsonList_)
+    jsonList_=filter_json_list(jsonList_, data[key][0])
+    data[key].extend(jsonList_)
+    remove_duplicate_by_name(data[key])
     save_json_to_file(data, pathFile)
-    remove_duplicate_by_name(data)
+    
     return jsonList_
     
 def getMonsterToPlace(place): 
     beast=open_json_file(beastPath)
-    level=open_json_file(levelPath)
     prompt=f"""
     Generate or match {place["beast_amount"]} examples
     Creatures that will match to place '{place["name"]}'
     Creatures are to be generated according to the given json from which you can match the creatures:
-    {select_values(beast["beast"], 50)}
+    {select_values(beast["beast"], 10)}
     when creating a description, I take care not to attribute the creature to a given region (in this case, {place["beast_amount"]})
     Generate a json list according to the example ( {place["beast_amount"]} example)
-    (You can also choose a matching spawn from the given list)
-    One last creature must be the boss
+    - You can also choose a matching spawn from the given list
+    - One last creature must be the boss
+    - It's important that you return me only json
     """
     completion=PM.chatCompletionCreate(prompt)
-    beasts_list=savePlaces_(completion,beast["beast"],beastPath)
+    beasts_list=savePlaces_(completion,beast,beastPath,"beast")
     update_place_with_beasts(place, beasts_list)
     
     
-places_(12)   
+ 
 # todo
 
 # monster=''
